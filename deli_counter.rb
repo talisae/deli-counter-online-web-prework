951b5c0a7dd805a0d7f0db6d504d5578471046be
Talisa Erazo <talisaerazo@gmail.com>
2018-10-18 03:10:26 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,10 +13,26 @@ def line(array)
   end
 end
 
-def take_a_number(line, name)
-  if line == 0
-  line.push("#{name}")
-  else katz_deli >= 1
-    newArray.push("#{name}")
-  end
+def take_a_number(array, name)
+    array.push(name)
+    position = array.index(name)
+      puts "Welcome, #{name}. You are number #{array.index(name)+ 1} in line."
+    return name, position
 end
+
+def take_a_number(array, name)
+    array.push(name)
+    position = array.index(name)
+      puts "Welcome, #{name}. You are number #{array.index(name)+1} in line."
+    return name, position 
+end 
+
+
+def now_serving(array)
+  if array.empty? == true
+    puts "There is nobody waiting to be served!" 
+  else array.empty? == false 
+    puts "Currently serving #{array.shift}."
+  end 
+end
+
