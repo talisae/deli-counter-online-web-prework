9c57b09ff0734d6f8059288f4487b84dfd1c663e
Talisa Erazo <talisaerazo@gmail.com>
2018-10-18 03:10:09 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+
+def line(array)
+  if array.length >= 1
+    newArray = []
+    counter = 1
+    array.each do |name|
+      newArray.push("#{counter}. #{name}")
+      counter += 1
+    end
+    puts "The line is currently: #{newArray.join(" ")}"
+  else
+    puts "The line is currently empty."
+  end
+end
+
+def take_a_number(line, name)
+  if line == 0
+  line.push("#{name}")
+  else katz_deli >= 1
+    newArray.push("#{name}")
+  end
+end
